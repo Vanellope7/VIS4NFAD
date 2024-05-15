b3363ca1b5185e9c34f1c039fa05cbe16c21098d
729149195 <729149195@qq.com>
2024-05-16 01:12:10 +0800

    brush

--- VIS4NFAD/src/components/Hcn-Data.vue
+++ VIS4NFAD/src/components/Hcn-Data.vue
@@ -1,168 +1,333 @@
 <template>
     <div ref="chartContainer" class="chart-container">
-        <div ref="chart"></div>
+      <div ref="chart"></div>
+      <div ref="slider" class="slider-container"></div>
     </div>
-</template>
-
-<script setup>
-import * as d3 from 'd3';
-import axios from 'axios';
-import { onMounted, ref } from 'vue';
-
-const chart = ref(null);
-
-onMounted(async () => {
-    const response = await axios.get('/data/hcnData.json'); // 请确保路径正确
-    const data = response.data;
-
-    const hcn = data.hcn;
-    const time = data.time;
-
-    const margin = { top: 20, right: 120, bottom: 40, left: 60 },
+  </template>
+  
+  <script setup>
+  import * as d3 from 'd3';
+  import axios from 'axios';
+  import { onMounted, ref } from 'vue';
+  
+  const chart = ref(null);
+  const slider = ref(null);
+  
+  onMounted(async () => {
+    try {
+      const response = await axios.get('/data/hcnData.json');
+      const data = response.data;
+  
+      const hcn = data.hcn;
+      const time = data.time;
+  
+      const margin = { top: 20, right: 120, bottom: 60, left: 60 },
         width = 1250 - margin.left - margin.right,
-        height = 600 - margin.top - margin.bottom;
-
-    const svg = d3.select(chart.value)
+        height = 600 - margin.top - margin.bottom,
+        sliderHeight = 50;
+  
+      const svg = d3.select(chart.value)
         .append("svg")
         .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
+        .attr("height", height + margin.top + margin.bottom);
+  
+      const clip = svg.append("defs").append("svg:clipPath")
+        .attr("id", "clip")
+        .append("svg:rect")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("x", 0)
+        .attr("y", 0);
+  
+      const chartArea = svg.append("g")
+        .attr("clip-path", "url(#clip)")
         .attr("transform", `translate(${margin.left},${margin.top})`);
-
-    // 计算所有数据的最小值和最大值
-    const xMin = d3.min(time) - 0.5;
-    const xMax = d3.max(time);
-    const yMin = d3.min(hcn, d => d3.min(d.filter(val => val !== null))) - 50;
-    const yMax = d3.max(hcn, d => d3.max(d.filter(val => val !== null))) + 50;
-
-    const x = d3.scaleLinear()
+  
+      const xMin = d3.min(time) - 0.5;
+      const xMax = d3.max(time);
+      const yMin = d3.min(hcn, d => d3.min(d.filter(val => val !== null))) - 50;
+      const yMax = d3.max(hcn, d => d3.max(d.filter(val => val !== null))) + 50;
+  
+      const x = d3.scaleLinear()
         .domain([xMin, xMax])
         .range([0, width]);
-
-    const y = d3.scaleLinear()
+  
+      const y = d3.scaleLinear()
         .domain([yMin, yMax])
         .range([height, 0]);
-
-    // 添加次刻度线
-    const xAxis = d3.axisBottom(x)
+  
+      const xAxis = d3.axisBottom(x)
         .ticks(10)
         .tickSizeOuter(0)
         .tickPadding(10)
         .tickFormat(d3.format(".1f"));
-
-    const yAxis = d3.axisLeft(y)
+  
+      const yAxis = d3.axisLeft(y)
         .ticks(10)
         .tickSizeOuter(0)
         .tickPadding(10)
         .tickFormat(d3.format("d"));
-
-    svg.append("g")
-        .attr("transform", `translate(0,${height})`)
+  
+      svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(${margin.left},${margin.top + height})`)
         .call(xAxis);
-
-    svg.append("g")
+  
+      svg.append("g")
+        .attr("class", "y-axis")
+        .attr("transform", `translate(${margin.left},${margin.top})`)
         .call(yAxis);
-
-    // 添加次刻度线
-    const xSubAxis = d3.axisBottom(x)
+  
+      const xSubAxis = d3.axisBottom(x)
         .ticks(100)
         .tickSize(-6)
         .tickFormat('');
-
-    const ySubAxis = d3.axisLeft(y)
+  
+      const ySubAxis = d3.axisLeft(y)
         .ticks(50)
         .tickSize(-6)
         .tickFormat('');
-
-    svg.append("g")
-        .attr("transform", `translate(0,${height})`)
+  
+      svg.append("g")
+        .attr("class", "x-sub-axis")
+        .attr("transform", `translate(${margin.left},${margin.top + height})`)
         .call(xSubAxis);
-
-    svg.append("g")
+  
+      svg.append("g")
+        .attr("class", "y-sub-axis")
+        .attr("transform", `translate(${margin.left},${margin.top})`)
         .call(ySubAxis);
-
-    // 添加网格线
-    const makeXGridlines = () => d3.axisBottom(x).ticks(10);
-    const makeYGridlines = () => d3.axisLeft(y).ticks(10);
-
-    svg.append("g")
-        .attr("class", "grid")
-        .attr("transform", `translate(0,${height})`)
+  
+      const makeXGridlines = () => d3.axisBottom(x).ticks(10);
+      const makeYGridlines = () => d3.axisLeft(y).ticks(10);
+  
+      svg.append("g")
+        .attr("class", "grid-x")
+        .attr("transform", `translate(${margin.left},${margin.top + height})`)
         .call(makeXGridlines()
-            .tickSize(-height)
-            .tickFormat(""))
+          .tickSize(-height)
+          .tickFormat(""))
         .selectAll("line")
         .style("stroke-dasharray", ("3, 3"))
-        .style("opacity", 0.3); // 设置网格线为虚线
-
-    svg.append("g")
-        .attr("class", "grid")
+        .style("opacity", 0.3);
+  
+      svg.append("g")
+        .attr("class", "grid-y")
+        .attr("transform", `translate(${margin.left},${margin.top})`)
         .call(makeYGridlines()
-            .tickSize(-width)
-            .tickFormat(""))
+          .tickSize(-width)
+          .tickFormat(""))
         .selectAll("line")
         .style("stroke-dasharray", ("3, 3"))
-        .style("opacity", 0.3); // 设置网格线为虚线
-
-    hcn.forEach((lineData, index) => {
-        svg.append("path")
-            .datum(lineData.map((d, i) => ({ x: time[i], y: d })).filter(d => d.y !== null))
-            .attr("fill", "none")
-            .attr("stroke", d3.schemeCategory10[index % 10])
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                .x(d => x(d.x))
-                .y(d => y(d.y)));
-
-        // 添加图例线条
+        .style("opacity", 0.3);
+  
+      svg.append("text")
+        .attr("transform", `translate(${margin.left + width / 2}, ${margin.top + height + margin.bottom - 10})`)
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .text("Time(s)");
+  
+      svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", margin.left - 40)
+        .attr("x", 0 - (margin.top + height / 2))
+        .attr("dy", "1em")
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .text("10^7/m^2");
+  
+      const focus = chartArea.append("g")
+        .attr("class", "focus")
+        .style("display", "none");
+  
+      focus.append("circle")
+        .attr("r", 4.5);
+  
+      focus.append("text")
+        .attr("x", 9)
+        .attr("dy", ".35em");
+  
+      svg.append("rect")
+        .attr("class", "overlay")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("pointer-events", "all")
+        .attr("transform", `translate(${margin.left},${margin.top})`)
+        .on("mouseover", () => focus.style("display", null))
+        .on("mouseout", () => focus.style("display", "none"))
+        .on("mousemove", mousemove);
+  
+      const bisectTime = d3.bisector(d => d).left;
+  
+      function mousemove(event) {
+        const x0 = x.invert(d3.pointer(event)[0]);
+        const i = bisectTime(time, x0, 1);
+        const d0 = time[i - 1];
+        const d1 = time[i];
+        const d = x0 - d0 > d1 - x0 ? d1 : d0;
+  
+        focus.attr("transform", `translate(${x(d)},${y(hcn[0][time.indexOf(d)])})`);
+        focus.select("text").text(d);
+      }
+  
+      hcn.forEach((lineData, index) => {
+        chartArea.append("path")
+          .datum(lineData.map((d, i) => ({ x: time[i], y: d })).filter(d => d.y !== null))
+          .attr("fill", "none")
+          .attr("stroke", d3.schemeCategory10[index % 10])
+          .attr("stroke-width", 1.5)
+          .attr("class", "line")
+          .attr("d", d3.line()
+            .x(d => x(d.x))
+            .y(d => y(d.y)));
+  
         svg.append("line")
-            .attr("x1", width + 10)
-            .attr("y1", index * 20 + 5)
-            .attr("x2", width + 30)
-            .attr("y2", index * 20 + 5)
-            .attr("stroke", d3.schemeCategory10[index % 10])
-            .attr("stroke-width", 5);
-
-        // 添加图例文本
+          .attr("x1", width + 10)
+          .attr("y1", index * 20 + 5)
+          .attr("x2", width + 30)
+          .attr("y2", index * 20 + 5)
+          .attr("stroke", d3.schemeCategory10[index % 10])
+          .attr("stroke-width", 5)
+          .attr("transform", `translate(${margin.left},${margin.top})`);
+  
         svg.append("text")
-            .attr("x", width + 35)
-            .attr("y", index * 20 + 10)
-            .attr("fill", "black")
-            .style("font-size", "10px")
-            .text(`${4043 + index}/hcn_ne001`);
-
-        // 添加 x 轴标签
-        svg.append("text")
-            .attr("transform", `translate(${width / 2}, ${height + margin.bottom})`)
-            .attr("text-anchor", "middle")
-            .style("font-size", "16px")
-            .text("Time(s)");
-
-        // 添加 y 轴标签
-        svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0 - margin.left + 15)
-            .attr("x", 0 - (height / 2))
-            .attr("text-anchor", "middle")
-            .style("font-size", "16px")
-            .text("10^7/m^2");
-    });
-});
-</script>
-
-<style scoped>
-.chart-container {
+          .attr("x", width + 35)
+          .attr("y", index * 20 + 10)
+          .attr("fill", "black")
+          .style("font-size", "10px")
+          .attr("transform", `translate(${margin.left},${margin.top})`)
+          .text(`${4043 + index}/hcn_ne001`);
+      });
+  
+      const zoom = d3.zoom()
+        .scaleExtent([1, 10])
+        .translateExtent([[0, 0], [width, height]])
+        .extent([[0, 0], [width, height]])
+        .on("zoom", zoomed);
+  
+      svg.append("rect")
+        .attr("class", "zoom")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("pointer-events", "all")
+        .attr("transform", `translate(${margin.left},${margin.top})`)
+        .call(zoom);
+  
+      function zoomed(event) {
+        const newX = event.transform.rescaleX(x);
+  
+        svg.selectAll(".x-axis").call(d3.axisBottom(newX).ticks(10));
+        svg.selectAll(".x-sub-axis").call(d3.axisBottom(newX).ticks(100).tickSize(-6).tickFormat(''));
+  
+        chartArea.selectAll(".line")
+          .attr("d", d3.line()
+            .x(d => newX(d.x))
+            .y(d => y(d.y)));
+  
+        svg.selectAll(".grid-x")
+          .call(makeXGridlines().scale(newX).tickSize(-height).tickFormat(""))
+          .selectAll("line")
+          .style("stroke-dasharray", ("3, 3"))
+          .style("opacity", 0.3);
+  
+        // 更新滑块
+        d3.select(slider.current).call(brush.move, newX.range().map(event.transform.invertX, event.transform));
+      }
+  
+      // 创建滑块
+      const sliderSvg = d3.select(slider.value)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", sliderHeight)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${sliderHeight / 4})`);
+  
+      const xSlider = d3.scaleLinear()
+        .domain([xMin, xMax])
+        .range([0, width]);
+  
+      sliderSvg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0,${sliderHeight / 4})`)
+        .call(d3.axisBottom(xSlider).ticks(10).tickFormat(d3.format(".1f")));
+  
+      const brush = d3.brushX()
+        .extent([[0, 0], [width, sliderHeight / 2]])
+        .on("brush end", brushed);
+  
+      sliderSvg.append("g")
+        .attr("class", "brush")
+        .call(brush)
+        .call(brush.move, x.range());
+  
+      function brushed(event) {
+        if (event.sourceEvent && event.sourceEvent.type === "zoom") return; // 忽略 zoom 事件触发的 brush 事件
+  
+        const selection = event.selection || xSlider.range();
+        const newX = selection.map(xSlider.invert, xSlider);
+  
+        x.domain(newX);
+        svg.selectAll(".x-axis").call(d3.axisBottom(x).ticks(10));
+        svg.selectAll(".x-sub-axis").call(d3.axisBottom(x).ticks(100).tickSize(-6).tickFormat(''));
+  
+        chartArea.selectAll(".line")
+          .attr("d", d3.line()
+            .x(d => x(d.x))
+            .y(d => y(d.y)));
+  
+        svg.selectAll(".grid-x")
+          .call(makeXGridlines().scale(x).tickSize(-height).tickFormat(""))
+          .selectAll("line")
+          .style("stroke-dasharray", ("3, 3"))
+          .style("opacity", 0.3);
+  
+        svg.call(zoom.transform, d3.zoomIdentity
+          .scale(width / (selection[1] - selection[0]))
+          .translate(-selection[0], 0));
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  });
+  </script>
+  
+  <style scoped>
+  .chart-container {
     width: 100%;
     height: 100%;
-}
-
-svg {
+  }
+  
+  .slider-container {
+    width: 100%;
+    height: 50px;
+  }
+  
+  svg {
     font: 10px sans-serif;
-}
-
-.axis path,
-.axis line {
+  }
+  
+  .axis path,
+  .axis line {
     fill: none;
     shape-rendering: crispEdges;
-}
-</style>
+  }
+  
+  .overlay {
+    fill: none;
+    pointer-events: all;
+  }
+  
+  .focus circle {
+    fill: none;
+    stroke: steelblue;
+  }
+  
+  .zoom {
+    cursor: move;
+    fill: none;
+    pointer-events: all;
+  }
+  </style>
+  
